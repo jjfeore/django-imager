00cea453b68ba4c30cab89d1201a9594ca7a10eb
James <jjfeore>
2017-06-20 10:49:55 -0700

    Couple small tweaks, still need tests

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -17,15 +17,18 @@ class ImagerActiveManager(models.Manager):
         """Return query of active users."""
         return super(ImagerActiveManager, self).get_queryset().filter(is_active=True)
 
+
 @python_2_unicode_compatible
-class Imager_Profile(models.Model):
+class ImagerProfile(models.Model):
     """A profile for users to our application."""
-    user = models.OneToOneField(User)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=255)
     age = models.IntegerField()
     camera_type = models.CharField(max_length=255)
     job = models.CharField(max_length=255)
     url = models.URLField(max_length=200)
+    objects = models.Manager()
     active = ImagerActiveManager()
 
     STYLE_CHOICES = (
@@ -58,30 +61,32 @@ class Imager_Profile(models.Model):
         default='COW',
     )
 
+    @property
     def is_active(self):
         """Return if profile is active."""
         return self.user.is_active
 
     def __str__(self):
         """Define a string representation of Imager_Profile."""
-        return '(Username: {}, Location: {}, Age: {}, Camera Type: {}, Job: {}, Social Status: {}, Style: {}, URL: {})'\
-            .format(
-                self.user,
-                self.location,
-                self.age,
-                self.camera_type,
-                self.job,
-                self.social_status,
-                self.photography_style,
-                self.url
-                )
+        return '(Username: {}, Location: {}, Age: {}, Camera Type: {}, \
+        Job: {}, Social Status: {}, Style: {}, URL: {})'\
+        .format(
+            self.user,
+            self.location,
+            self.age,
+            self.camera_type,
+            self.job,
+            self.social_status,
+            self.photography_style,
+            self.url
+        )
 
 
 @receiver(post_save, sender=User)
 def make_profile_for_new_user(sender, **kwargs):
     """Instantiate profile for new user."""
     if kwargs['created']:
-        new_profile = Imager_Profile(
+        new_profile = ImagerProfile(
             user=kwargs['instance']
         )
         new_profile.save()
