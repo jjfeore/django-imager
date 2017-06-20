7b6c5138a436e742326e878f6c0e01042c5d6311
ADL175 <armydavidlim@gmail.com>
2017-06-20 09:25:47 -0700

    another commit for code review

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -5,10 +5,19 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
 
+class ImagerActiveManager(models.Manager):
+    """Manager for the ImagerProfile Active API."""
+
+    def get_queryset(self):
+        """Return query of active users."""
+        return super(ImagerActiveManager, self).get_queryset().filter(is_active=True)
+
+@python_2_unicode_compatible
 class Imager_Profile(models.Model):
     """A profile for users to our application."""
     user = models.OneToOneField(User)
@@ -17,6 +26,7 @@ class Imager_Profile(models.Model):
     camera_type = models.CharField(max_length=255)
     job = models.CharField(max_length=255)
     url = models.URLField(max_length=200)
+    active = ImagerActiveManager()
 
     STYLE_CHOICES = (
         ('MONO', 'Monochrome'),
@@ -48,8 +58,23 @@ class Imager_Profile(models.Model):
         default='COW',
     )
 
-    def __repr__(self):
-        return self.user.username
+    def is_active(self):
+        """Return if profile is active."""
+        return self.user.is_active
+
+    def __str__(self):
+        """Define a string representation of Imager_Profile."""
+        return '(Username: {}, Location: {}, Age: {}, Camera Type: {}, Job: {}, Social Status: {}, Style: {}, URL: {})'\
+            .format(
+                self.user,
+                self.location,
+                self.age,
+                self.camera_type,
+                self.job,
+                self.social_status,
+                self.photography_style,
+                self.url
+                )
 
 
 @receiver(post_save, sender=User)
