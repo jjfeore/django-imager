365a5cba0d85eb2119cbf588f99db7d86bea8d54
ADL175 <armydavidlim@gmail.com>
2017-06-23 17:29:53 -0700

    working on imagersite views

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,12 +1,18 @@
 """Define views for imagersite."""
 
-
+from random import randint
 from django.shortcuts import render
-
+from imager_images.models import Photo
 
 def home_view(request):
     """Home view callable, for the home page."""
-    context = {'page': ' | Home'}
+    count = Photo.objects.count()
+    r_num = randint(0, count - 1)
+    image = Photo.objects.all()[r_num]
+    context = {
+        'page': ' | Home',
+        'image': image
+    }
     return render(request, 'imagersite/home.html', context=context)
 
 
