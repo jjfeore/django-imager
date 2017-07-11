5658cfa934a0ed18f18f6fa6c536f782ceecdc27
James <jjfeore>
2017-07-10 17:01:28 -0700

    Removed hard coded URLs

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/images/library/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
