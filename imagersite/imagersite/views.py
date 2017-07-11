247b99eafef6ae908bedda1278bb8cca29840be3
James <jjfeore>
2017-07-10 19:21:29 -0700

    Debugging the front end

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,17 +1,17 @@
 """Define views for imagersite."""
 
-from random import randint
+from random import randrange
 from django.shortcuts import render
 from imager_images.models import Photo
 
 
 def home_view(request):
     """Home view callable, for the home page."""
-    count = Photo.objects.count()
-    r_num = randint(0, count - 1)
     try:
+        count = Photo.objects.count()
+        r_num = randrange(0, count)
         image = Photo.objects.all()[r_num]
-    except IndexError:
+    except ValueError:
         image = None
     context = {
         'page': ' | Home',
