808b8e1d727a34a67678e94a313df4e7ef83be07
James <jjfeore>
2017-07-13 15:31:52 -0700

    More class-based views work

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,12 +1,11 @@
 """Views for photos, albums, and library."""
 
 
-from django.http import HttpResponseForbidden
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView, TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from imager_images.models import Album, Photo
-from imager_profile.models import ImagerProfile
-from django.contrib.auth.models import User
-from django.conf import settings
 
 
 def library_view(request):
@@ -54,3 +53,77 @@ def album_detail_view(request, pk):
         album = Album.objects.get(pk=pk)
         photos = album.in_album.all()
         return render(request, "imager_images/album_detail.html", {"photos": photos, "album": album})
+
+
+class LibraryView(LoginRequiredMixin, TemplateView):
+    """View for the library."""
+
+    template_name = "imager_images/library.html"
+    login_url = reverse_lazy("login")
+
+    def get_context_data(self):
+        user = self.request.user
+        album_list = user.albums.all()
+        photo_list = user.photos.all()
+        return {"albums": album_list, "photos": photo_list}
+
+
+class PhotosView(ListView):
+    """View all the photos."""
+
+    template_name = "imager_images/photos.html"
+
+    def get_context_data(self):
+        user = self.request.user
+        photo_list = user.photos.all().filter(published='PUB')
+        return {"photos": photo_list}
+
+
+class AlbumsView(ListView):
+    """View all albums."""
+
+    template_name = "imager_images/albums.html"
+    model = Album
+    context_object_name = "albums"
+
+    def get_queryset(self):
+        """Modify get_queryset to return list of published albums for specific user."""
+        return Album.published_albums.filter(owner=self.request.user)
+
+
+class AddPhotoView(LoginRequiredMixin, CreateView):
+    """Add a Photo to the user."""
+
+    login_url = reverse_lazy("login")
+    template_name = "imager_images/add_photo.html"
+    model = Photo
+    fields = [
+        "title", "description", "published", "date_published", "image"
+    ]
+
+    def form_valid(self, form):
+        """Force the form to use the current user as the author."""
+        form.instance.author = self.request.user
+        photo = form.save()
+        photo.author = self.request.user
+        photo.save()
+        return redirect("/images/library/")
+
+
+class AddAlbumView(LoginRequiredMixin, CreateView):
+    """Add an Album to the user."""
+
+    login_url = reverse_lazy("login")
+    template_name = "imager_images/add_album.html"
+    model = Album
+    fields = [
+        "title", "description", "album_cover", "published", "date_published", "pictures"
+    ]
+
+    def form_valid(self, form):
+        """Force the form to use the current user as the author."""
+        form.instance.owner = self.request.user
+        album = form.save()
+        album.owner = self.request.user
+        album.save()
+        return redirect("/images/library/")
